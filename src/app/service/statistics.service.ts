720684f024478bcdc358e148b03683a1de6a1171
Tim Schuppener <tim@schuppener.de>
2019-02-08 21:33:52 +0100

    a more generic approach to interval-tasks
    making it easier to add more of this

--- src/app/service/statistics.service.ts
+++ src/app/service/statistics.service.ts
@@ -1,28 +1,15 @@
 import { Injectable } from '@angular/core';
 import { IpfsService } from './ipfs.service';
 import { SidebarService } from './sidebar.service';
+import { PausableIntervalTask } from '../util/pausableIntervalTask';
 
 @Injectable({
     providedIn: 'root'
 })
 export class StatisticsService {
     private runStatistics = false;
-    
-    constructor(
-        private ipfs: IpfsService,
-        public sidebar: SidebarService
-    ) {
-        sidebar.topic.subscribe(async whatToShow => {
-            this.runStatistics = (whatToShow === 'statistics');
-            if (this.runStatistics) {
-                this.updateStatsBw();
-            }
-        });
-    }
-
-    public statsBw = null;
-    private async updateStatsBw() {
-        while (this.runStatistics) {
+    private readonly allStats =  [
+        new PausableIntervalTask(async () => {
             const statisticsFrom = await this.ipfs.ipfs.stats.bw();
             this.statsBw = [
                 {
@@ -38,10 +25,29 @@ export class StatisticsService {
                     key: 'outgoing rate', value: this.bytesToReadable(statisticsFrom.rateOut) + '/s'
                 }
             ];
-            await new Promise(resolve => setTimeout(resolve, 2000));
-        }
+        }, 2000),
+    ];
 
+    constructor(
+        private ipfs: IpfsService,
+        public sidebar: SidebarService
+    ) {
+        sidebar.topic.subscribe(async whatToShow => {
+            const start = (whatToShow === 'statistics');
+            if (start) {
+                this.runStatistics = true;
+                this.allStats.forEach(f => f.start());
+            } else {
+                if (this.runStatistics) {
+                    this.runStatistics = false;
+                    this.allStats.forEach(f => f.stop());
+                }
+            }
+        });
     }
+
+    public statsBw = null;
+
     private bytesToReadable(incoming) {
         let suffix = 'B';
         if (incoming > 1024) {
---
+++ src/app/util/pausableIntervalTask.ts
@@ -0,0 +1,19 @@
+import Semaphore from 'semaphore-async-await';
+import { mutexify } from './mutex';
+
+export class PausableIntervalTask {
+    private intervalId = null;
+    constructor(private func, private ms: number) { }
+
+    async start() {
+        if (this.intervalId) { throw new Error('is already running'); }
+        this.intervalId = setInterval(this.func, this.ms);
+        this.func();
+
+    }
+    async stop() {
+        if (this.intervalId === null) { throw new Error('has not been started yet'); }
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+}
