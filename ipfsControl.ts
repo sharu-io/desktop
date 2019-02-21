edd5b440c482988f69a59296875fcd9ac55e1a8d
Tim Schuppener <tim@schuppener.de>
2019-02-21 21:19:01 +0100

    adding go-ipfs-node (and fixing duplicates)

--- ipfsControl.ts
+++ ipfsControl.ts
@@ -281,8 +281,6 @@ export function generateIpfsConfig(forcedPath?: string) {
             'Gateway': '/ip4/127.0.0.1/tcp/9090'
         },
         'Bootstrap': [
-            '/ip4/159.89.6.248/tcp/4004/ipfs/QmSmCMn5Mq5sLSa6mehmUtFLmqezu7mXHHPepvXiYTX1vR',
-            '/ip4/159.89.6.248/tcp/4001/ipfs/Qme1rMSWymeMZKaLMuXWEAXz7YXXwiWfnwc3UiSA95yGQ1',
             '/ip4/213.165.80.135/tcp/4001/ipfs/QmXPiBKHQ31s33n6F9cUWYjdGSFs5nAzxBPoq6NqnrR1uj',
             '/ip4/104.236.176.52/tcp/4001/ipfs/QmSoLnSGccFuZQJzRadHn95W2CrSFmZuTdDWP8HXaHca9z',
             '/ip4/104.131.131.82/tcp/4001/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
@@ -322,7 +320,9 @@ export function generateIpfsConfig(forcedPath?: string) {
 
     const custom = getCustomBootStrapAdditions(forcedPath);
     custom.Bootstrap.forEach(peer => {
-        stdConfig.Bootstrap.push(peer);
+        if (!stdConfig.Bootstrap.includes(peer)){
+            stdConfig.Bootstrap.push(peer);
+        }
     });
 
     return stdConfig;
@@ -343,6 +343,8 @@ export function getCustomBootStrapAdditions(forcedPath?: string) {
         fs.writeFileSync(pathToCustomBootstrapAdditions, JSON.stringify(bootStrap));
         console.log('created default custom bootstrap config: ' + pathToCustomBootstrapAdditions);
     }
+    // these are the nodes provided by sharu
     bootStrap.Bootstrap.push('/ip4/159.89.6.248/tcp/4004/ipfs/QmSmCMn5Mq5sLSa6mehmUtFLmqezu7mXHHPepvXiYTX1vR');
+    bootStrap.Bootstrap.push('/ip4/159.89.6.248/tcp/4001/ipfs/Qmci7GnY4WvEJr1UFBAfMqji7Vch1cEpAcGgfVGHVKxeJp');
     return bootStrap;
 }
