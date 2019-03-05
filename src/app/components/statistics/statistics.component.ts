9d4c719c5faa35fc8be9e4475f2529d64d2663cf
Tim Schuppener <tim@schuppener.de>
2019-03-05 17:26:18 +0100

    added support for dht.findProvs (not working as DHT is not there)

--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { StatisticsService } from '../../service/statistics.service';
 import { IpfsService } from '../../service/ipfs.service';
+import { ToastService } from '../../service/toast.service';
 
 @Component({
     // tslint:disable-next-line:component-selector
@@ -13,9 +14,26 @@ export class StatisticsComponent {
     public peerFilter = '';
     public selectedPeer = null;
 
+    public searchHash = '';
+    public findHash: {search: string, result: string}  = null;
 
-    constructor(public stats: StatisticsService, public ipfs: IpfsService) { }
+
+    constructor(public stats: StatisticsService, public ipfs: IpfsService, public toast: ToastService) { }
 
     displayedColumnsBandWith: string[] = ['key', 'value'];
     displayedColumnsPeers: string[] = ['peer'];
+
+    public async lookupHash(hash: string) {
+        this.findHash = {
+            search: hash,
+            result: null
+        };
+        try {
+            this.findHash.result = await this.ipfs.ipfs.dht.findProvs(hash);
+        } catch (error) {
+            this.toast.notify('error', `findHash(${hash}) failed`, error);
+            console.error(error);
+            this.findHash = null;
+        }
+    }
 }
